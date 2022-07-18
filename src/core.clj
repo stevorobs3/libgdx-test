89d49accee8c66e4c633ef157470505711c5fcbe
Steve Robinson-Burns <steverb42@gmail.com>
2022-07-18 16:17:55 +0100

    Input processor example

--- src/core.clj
+++ src/core.clj
@@ -1,45 +1,31 @@
 (ns core
   (:import
-    [com.badlogic.gdx Game Gdx Input$Keys Input$Buttons]
+    [com.badlogic.gdx Game Gdx Input$Keys Input$Buttons InputAdapter]
     [com.badlogic.gdx.graphics GL20]
     (com.badlogic.gdx.graphics.glutils ShapeRenderer ShapeRenderer$ShapeType)))
 
-(defn- render [shape-renderer delta-time {:keys [position radius]}]
-  (let [circle-x (cond-> (:x position)
-                         (.isButtonPressed Gdx/input Input$Buttons/LEFT) ((fn [x] (.getX Gdx/input)))
-                         (.isKeyPressed Gdx/input Input$Keys/A) dec
-                         (.isKeyPressed Gdx/input Input$Keys/D) inc)
-        circle-y (cond-> (:y position)
-                         (.isButtonPressed Gdx/input Input$Buttons/LEFT) ((fn [y] (- (.getHeight Gdx/graphics) (.getY Gdx/input))))
-                         (.isKeyPressed Gdx/input Input$Keys/W) inc
-                         (.isKeyPressed Gdx/input Input$Keys/S) dec)
-        radius   50]
-
-
-
-    (.glClearColor Gdx/gl (float 0.25) 0.25 (float 0.25) (float 1))
-    (.glClear Gdx/gl GL20/GL_COLOR_BUFFER_BIT)
-    (.begin shape-renderer ShapeRenderer$ShapeType/Filled)
-    (.setColor shape-renderer 0 0 1 1)
-
-    (.circle shape-renderer circle-x circle-y radius)
-    (.end shape-renderer)
-    {:position {:x circle-x
-                :y circle-y}
-     :radius   radius}))
+(defn- render [{:keys [r g b] :as state}]
+  (.glClearColor Gdx/gl r g b 1)
+  (.glClear Gdx/gl GL20/GL_COLOR_BUFFER_BIT)
+  state
+  )
 
 (defn create-game []
-  (let [shape-renderer (atom nil)
-        state          (atom {:position {:x 400
-                                         :y 300}
-                              :radius   50})]
+  (let [rand-float #(float (/ (rand-int Integer/MAX_VALUE) Integer/MAX_VALUE))
+        new-state  #(hash-map :r (rand-float)
+                              :g (rand-float)
+                              :b (rand-float))
+        state      (atom (new-state))]
     (proxy [Game] []
       (create []
         (prn "creating game!")
-        (swap! shape-renderer (constantly (ShapeRenderer.)))
-        )
+        (.setInputProcessor Gdx/input
+                            (proxy [InputAdapter] []
+                              (keyTyped [char]
+                                (prn "typed" char (type char))
+                                (swap! state (fn [_] (new-state)))
+                                true))))
       (render []
         (swap! state (fn [s]
-                       (render @shape-renderer (.getDeltaTime Gdx/graphics) s))))
-      (dispose []
-        (.dispose @shape-renderer)))))
+                       (render s))))
+      )))

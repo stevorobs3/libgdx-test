f74b4359f724754918014d58d652a81d59fb123d
Steve Robinson-Burns <steverb42@gmail.com>
2022-07-25 11:57:40 +0100

    Add all viewports

--- src/core.clj
+++ src/core.clj
@@ -4,13 +4,13 @@
     (com.badlogic.gdx.graphics.g2d BitmapFont SpriteBatch)
     (com.badlogic.gdx.graphics OrthographicCamera Texture)
     (com.badlogic.gdx.graphics.glutils ShapeRenderer)
-    (com.badlogic.gdx.utils.viewport FitViewport FillViewport)))
+    (com.badlogic.gdx.utils.viewport FitViewport FillViewport StretchViewport ExtendViewport)))
 
 (defn create-game [create-initial-screen-fn]
   (let [[font batch shape-renderer justina-texture
          :as disposables] (repeatedly 4 (fn [] (atom nil)))
         camera       (atom nil)
-        view-port    (atom nil)
+        view-ports   (atom nil)
         world-width  1920
         world-height 1080]
     (proxy [Game] []
@@ -20,15 +20,35 @@
                              (reset! font (BitmapFont.))
                              (reset! batch (SpriteBatch.))
                              (reset! justina-texture (Texture. "justina.jpg"))
-                             (let [camera (let [c (doto (OrthographicCamera.)
-                                                    (.update))]
-                                            (.set (.position c) (/ world-width 2) (/ world-height 2) 0)
-                                            c)]
-                               (reset! shape-renderer (ShapeRenderer.))
-                               (reset! view-port (let [vp (FitViewport. world-width world-height camera)]
+                             (reset! shape-renderer (ShapeRenderer.))
+                             (reset! view-ports [(let [camera (doto (OrthographicCamera.)
+                                                                (.update))
+                                                       vp     (FitViewport. world-width world-height camera)]
+                                                   (.set (.position camera) (/ world-width 2) (/ world-height 2) 0)
                                                    (.apply vp)
                                                    (.update vp world-width world-height true)
-                                                   vp)))
+                                                   vp)
+                                                 (let [camera (doto (OrthographicCamera.)
+                                                                (.update))
+                                                       vp     (FillViewport. world-width world-height camera)]
+                                                   (.set (.position camera) (/ world-width 2) (/ world-height 2) 0)
+                                                   (.apply vp)
+                                                   (.update vp world-width world-height true)
+                                                   vp)
+                                                 (let [camera (doto (OrthographicCamera.)
+                                                                (.update))
+                                                       vp     (StretchViewport. world-width world-height camera)]
+                                                   (.set (.position camera) (/ world-width 2) (/ world-height 2) 0)
+                                                   (.apply vp)
+                                                   (.update vp world-width world-height true)
+                                                   vp)
+                                                 (let [camera (doto (OrthographicCamera.)
+                                                                (.update))
+                                                       vp     (ExtendViewport. world-width world-height camera)]
+                                                   (.set (.position camera) (/ world-width 2) (/ world-height 2) 0)
+                                                   (.apply vp)
+                                                   (.update vp world-width world-height true)
+                                                   vp)])
                              {:game            this
                               :font            @font
                               :batch           @batch
@@ -36,7 +56,8 @@
                               :world-height    world-height
                               :justina-texture @justina-texture
                               :shape-renderer  @shape-renderer
-                              :view-port       @view-port})))
+                              :view-port       (first @view-ports)
+                              :view-ports      @view-ports})))
         (println "creating game!"))
       (dispose []
         (println "disposing" (count disposables) "things")
